01f4442665c1cae278985fc32c9b5265874745d5
ijefin <jeffersonmecmiler@hotmail.com>
2022-10-25 17:13:54 -0300

    finished desktop login page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,29 +1,29 @@
 import { Component } from "react";
-import LogoutIcon from "@mui/icons-material/Logout";
+import LogoDente from "../assets/images/newDentes.png";
+
 export default class Login extends Component {
   render() {
     return (
       <>
-        <div className="container p-5">
+        <div className="container-fluid">
           <div className="row">
-            <div className="col-md-8 myBox  bg-secondary">
-              <h1 className="text-white">OdontoDev</h1>
+            <div className="col-md-8 myBox bg-light">
+              <img src={LogoDente} alt="logo" className="img-fluid" />
             </div>
-            <div className="col bg-primary">
-              <div className="card text-white bg-primary mb-3">
-                <div className="card-body">
-                  <h4 className="card-title">Acessar Sistema</h4>
-                </div>
-                <div className="row">
+            <div className="col bg-info myBox ">
+              <div className=" text-white mb-3">
+                <div className="card-body"></div>
+                <div className="row justify-content-center align-items-center">
                   <div className="col-sm-8">
+                    <h1 className="card-title mb-5">Login</h1>
                     <div className="form-group pb-3">
                       <label>
-                        <h4>Usuário</h4>
+                        <h5>Usuário</h5>
                       </label>
                       <input
                         type="text"
                         name="nome"
-                        placeholder=""
+                        placeholder="Seu email"
                         className="form-control"
                       />
                     </div>
@@ -31,15 +31,19 @@ export default class Login extends Component {
                   <div className="col-sm-8">
                     <div className="form-group">
                       <label>
-                        <h4>Senha</h4>
+                        <h5>Senha</h5>
                       </label>
-                      <h1>{LogoutIcon}</h1>
                       <input
-                        type="text"
+                        type="password"
                         name="sku"
-                        placeholder={""}
+                        placeholder="Sua senha"
                         className="form-control"
                       />
+                    </div>
+                  </div>
+                  <div className="col-sm-8">
+                    <div className="form-group">
+                      <button className="form-control mt-3">Entrar</button>
                     </div>
                   </div>
                 </div>
